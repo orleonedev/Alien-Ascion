e5fe1f2301ff6c97a53822b17aaaeb3ccec87792
orleonedev <orleone.dev@gmail.com>
2021-10-11 20:25:17 +0200

    animazioni ok (?) tentativo cambio view

--- Alien-Ascion.playground/Pages/Chapter1.xcplaygroundpage/Contents.swift
+++ Alien-Ascion.playground/Pages/Chapter1.xcplaygroundpage/Contents.swift
@@ -1,22 +1,46 @@
 import SwiftUI
 import PlaygroundSupport
+import UIKit
 
-struct ContentView: View {
+struct IntroView: View {
+    @State var introOpacity = 0.0
     var body: some View {
         ZStack{
-            Rectangle()
+            Rectangle().fill().foregroundColor(Color.init(red: 0.01, green: 0.01, blue: 0.1))
             
-            VStack(alignment: .center){
-                Text("Once upon a time in a far far galaxy...")
-                    .foregroundColor(.white)
+            VStack{
+                Text("A long time ago in a galaxy far, far away...")
                     .font(.body)
+                    .foregroundColor(.cyan)
+                    .padding()
+                    .opacity(introOpacity)
+                    .onAppear{
+                        let fadein = Animation.easeIn(duration: 5)
+                        withAnimation(fadein){
+                            introOpacity = 1.0
+                        }
+                    }
+                    .onDisappear{
+                        let fadeOut = Animation.easeIn(duration: 5)
+                        withAnimation(fadeOut){
+                            introOpacity = 0.0
+                        }
+                    }
+            }.onTapGesture {
+                Scene1()
             }
-            
-        }.background(Color.black)
-        .frame(width: 600, height: 400)
+    }.frame(width: 600, height: 400, alignment: .center)
     }
-    
 }
 
-PlaygroundPage.current.setLiveView(ContentView())
 
+Struct Scene1: View {
+    var body: some View{
+        ZStack{
+            Rectangle().fill().foregroundColor(.blue)
+        }.frame(width: 600, height: 400, alignment: .center)
+    }
+}
+
+let viewController = UIHostingController(rootView: IntroView())
+PlaygroundPage.current.setLiveView(viewController)
--- Alien-Ascion.playground/Pages/Chapter4.xcplaygroundpage/Contents.swift
+++ Alien-Ascion.playground/Pages/Chapter4.xcplaygroundpage/Contents.swift
@@ -1,5 +1,61 @@
-//: [Previous](@previous)
-
-//fffffffffff cncmgm
-
-//: [Next](@next)
+//@State var introState = false
+//@State var dissolve = false
+//@State var scene1 = false
+//@State var scene2 = false
+//
+//ZStack{
+//    // Background
+//    Rectangle().fill().foregroundColor(Color.init(red: 0.05, green: 0.05, blue: 0.1))
+//    //Testo Iniziale
+//    if introState{
+//        VStack(alignment: .center){
+//            Text("Once upon a time in a far far galaxy...")
+//                .font(.body)
+//                .foregroundColor(.cyan)
+//                .padding()
+//                .animation(.easeIn, value: dissolve)
+////                        .onAppear(){
+////                            withAnimation(Animation.easeInOut(duration: 0.6)){
+////                                dissolve.toggle()
+////                            }
+////                        }.opacity(dissolve ? 1 : 0)
+//                .onTapGesture{
+//                    print("Introstate change from -> \(introState)")
+//                    introState=false
+//                    print("to ->\(introState)")
+//                    scene1=true
+//                }
+//                }
+//        }
+//    /*
+//    //Scena 1
+//    if scene1{
+//        //gestore layer
+//        ZStack{
+//            //Livello immagini
+//            //TODO
+//            //Overlay testo
+//            VStack(alignment: .center){
+//                //per mantenere in basso il testo
+//                Spacer()
+//                //Layer back-testo
+//                ZStack{
+//                    //background
+//                    Rectangle().frame(width: 600, height: 100, alignment: .center).foregroundColor(Color.init( red: 1.0, green: 1.0, blue: 1.0, opacity: 0.08)).padding().blur(radius: 10)
+//                // Testo
+//                    Text("There was Alien Ascion, belonging to the race of the Grays. But he was special: in fact, he was Blue, and for this reason he was not look kindly upon by others and even his own family tended to exclude him, mock him, make him feel different.")
+//                        .padding(20)
+//                        .foregroundColor(.white.opacity(0.9))
+//                        .font(.body)
+//                }
+//            }
+//        }
+////            }*/
+//}
+//.frame(width: 600, height: 400)
+//.onAppear {
+//        print("Introstate change from -> \(introState)")
+//        introState=true
+//        print("to ->\(introState)")
+//    }
+//}
